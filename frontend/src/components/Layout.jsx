ac1d3a05475bbc7a1221a9c6dd6146a536cc4565
Emerson Lima <emersonlimago123@gmail.com>
2024-04-14 20:50:20 -0300

    layout: added progress bar on route change

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,9 +1,37 @@
 import Header from "../components/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import TopBarProgress from "react-topbar-progress-indicator";
+
+TopBarProgress.config({
+  barColors: {
+    0: "#833ab4",
+    0.5: "#1d5afd",
+    1.0: "#fc459f",
+  },
+  barThickness: 10,
+});
 
 function Layout() {
+  const [progress, setProgress] = useState(false);
+  const [prevLoc, setPrevLoc] = useState("");
+  let location = useLocation();
+
+  useEffect(() => {
+    setPrevLoc(location.pathname);
+    setProgress(true);
+    if (location.pathname === prevLoc) {
+      setPrevLoc("");
+    }
+  }, [location]);
+
+  useEffect(() => {
+    setProgress(false);
+  }, [prevLoc]);
+
   return (
     <>
+      {progress && <TopBarProgress />}
       <Header />
       <Outlet />
     </>
